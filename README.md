65aa7a37568390a03faa7ff496a33926e05edfbf
natural-light <gibson.lee@okstate.edu>
2026-08-21 09:41:28 -0500

    first day ever seeing python ever!!!!!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ENGR-1412
-all of my work that i do in okstate's engr-1412 class
+self-explanatory
---
+++ week 1/8-20.py
@@ -0,0 +1,5 @@
+"""
+lots of syllabus things today
+3 wks in excel, then the rest are in python
+ok i didn't need to get my laptop out
+"""
