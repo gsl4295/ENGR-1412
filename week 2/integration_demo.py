450f7a6553466bb44bd8ad2c988cd9cb3dc05efc
natural-light <gibson.lee@okstate.edu>
2026-08-29 17:11:50 -0500

    peak.

--- week 2/integration_demo.py
+++ week 2/integration_demo.py
@@ -1,4 +1,7 @@
 # unnecessarily doing calculus outside of class. we'll say it's for honors credit (it's not)
+import csv
+import matplotlib.pyplot
+import pandas
 
 class Function:
     def __init__(self, function: str, dx: float, bounds: tuple):
@@ -36,10 +39,32 @@ class Function:
         print(f"dx = {self.dx}")
         print(f"function = {self.function}")
         print(f"bounds = {self.domain}")
-        print("--------------- Results ----------------")
+        print("\n--------------- Results ----------------")
         print(f"area under curve = {round(self.area_under_curve, 10)}")
+        print("\n--------------- Options ----------------")
+        graphing_option = input("(y/n) - graph results? - ")
+        if graphing_option == "y":
+            self.convert_to_csv()
+            self.graph_results()
 
-f = Function("(x*x*x)", 0.001, (-10, 0)) # hardcoding this for now. it'll have text input as an optional thing at some point.
+    def convert_to_csv(self):
+        header = ["x","y"]
+        data = []
+        for row in range(len(self.x)):
+            data.append([round(self.x[row], 10), round(self.y[row], 10)])
+        with open('integration_output.csv', 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(header)
+            writer.writerows(data)
+
+    @staticmethod
+    def graph_results():
+        graph = pandas.read_csv('integration_output.csv')
+        graph.plot(x="x", y="y")
+        matplotlib.pyplot.grid()
+        matplotlib.pyplot.show() # i did it!!!
+
+f = Function("(x*x*x)", 0.001, (-1, 1)) # hardcoding this for now. it'll have text input as an optional thing at some point.
 f.evaluate_function()
 f.integrate()
 f.report_results()
