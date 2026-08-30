6b5670392f30053861877846e27016024d8e7d0e
natural-light <gibson.lee@okstate.edu>
2026-08-29 23:57:18 -0500

    really, really nice code. dx is now optimized to 0.00001% accuracy.

--- week 2/integration_demo.py
+++ week 2/integration_demo.py
@@ -8,14 +8,17 @@ import matplotlib.pyplot
 import pandas
 
 class Function:
-    def __init__(self, function: str, dx: float, domain: tuple):
+    def __init__(self, function: str, domain: tuple):
         self.function = function
-        self.dx = dx
         self.domain = domain
         self.x = []
         self.y = []
         self.approximations = []
         self.area_under_curve = 0
+        self.dx = self.domain[1] - self.domain[0]
+        self.dx_results = {}
+        self.counter = 0
+        self.calculation_counter = 0
 
     def evaluate_function(self):
         for i in range(int((self.domain[1] - self.domain[0]) / self.dx) + 1):
@@ -37,14 +40,44 @@ class Function:
                 trapezoidal_area = round((x2 - x1) * (y2 + y1) / 2, 10)
                 self.approximations.append(trapezoidal_area)
                 self.area_under_curve += trapezoidal_area
+        return round(self.area_under_curve, 10)
+
+    def reset_results(self):
+        self.area_under_curve = 0
+        self.x.clear()
+        self.y.clear()
+        self.calculation_counter += len(self.approximations)
+        self.approximations.clear()
+
+    def dx_optimizer(self):
+        self.counter += 1
+        self.dx *= (1/2)
+        self.dx = round(self.dx, 10)
+        self.evaluate_function()
+        self.dx_results[self.counter] = self.integrate()
+        if self.counter == 1:
+            self.reset_results()
+            self.dx_optimizer()
+        else:
+            previous_area = self.dx_results[self.counter - 1]
+            current_area = self.dx_results[self.counter]
+            percent_area_difference = abs(current_area / previous_area)
+            if percent_area_difference < 0.99999:
+                self.reset_results()
+                self.dx_optimizer()
+            else:
+                self.calculation_counter += len(self.approximations)
+                self.report_results()
 
     def report_results(self):
         print("--------------- Details ----------------")
         print(f"function = {self.function}")
-        print(f"dx = {self.dx}")
         print(f"domain = {self.domain}")
+        print(f"total trapezoids calculated = {self.calculation_counter}")
+        print(f"trapezoids in this graph = {len(self.approximations)}")
         print("\n--------------- Results ----------------")
-        print(f"area under curve = {round(self.area_under_curve, 10)}")
+        print(f"optimized dx = {self.dx}")
+        print(f"approximated area under curve = {round(self.area_under_curve, 10)}")
         print("\n--------------- Options ----------------")
         graphing_option = input("(y/n) - graph results? - ")
         if graphing_option == "y":
@@ -52,7 +85,7 @@ class Function:
             self.graph_results()
 
     def convert_to_csv(self):
-        header = ["x","y"]
+        header = ["x","f(x)"]
         data = []
         for row in range(len(self.x)):
             data.append([round(self.x[row], 10), round(self.y[row], 10)])
@@ -61,14 +94,11 @@ class Function:
             writer.writerow(header)
             writer.writerows(data)
 
-    @staticmethod
-    def graph_results():
+    def graph_results(self):
         graph = pandas.read_csv('integration_output.csv')
-        graph.plot(x="x", y="y")
+        graph.plot(x="x", y="f(x)", title=f"dx = {self.dx}  |  A = {round(self.area_under_curve, 10)}")
         matplotlib.pyplot.grid()
-        matplotlib.pyplot.show() # i did it!!!
+        matplotlib.pyplot.show() # i did it!!! aaaaaaaaaa
 
-f = Function("(x*x)", 0.1, (0, 1)) # hardcoding this for now. it'll have text input as an optional thing at some point.
-f.evaluate_function()
-f.integrate()
-f.report_results()
+f = Function("(x*x)", (-1, 1)) # hardcoding this stuff for now. it'll have text input as an optional thing at some point.
+f.dx_optimizer()
